61b47ae36bb86d5c6c4ae296720c50756d730b2e
agyeievans <evanskofiagyei06@gmail.com>
2023-04-05 12:21:23 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Quizzes</h1>
+      <input type="text" placeholder="Serach....">
+    </header>
+
+    <div class="options-container">
+      <div class="card">
+        <img src="https://img.freepik.com/free-vector/chalkboard-with-math-elements_1411-88.jpg" alt="">
+        <div class="card-text">
+          <h2>Math</h2>
+          <p>15 questions</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.container{
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header{
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1{
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input{
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+/* CARDS*/
+
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+</style>
