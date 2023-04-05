65542d2c19e2fe6c954c88f3b2010e7d7e0b36f0
agyeievans <evanskofiagyei06@gmail.com>
2023-04-05 17:42:04 +0000

    added Card component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
-  import q from "./data/quizzes.json"
-  import {ref, watch} from "vue"
+  import q from "./data/quizzes.json";
+  import {ref, watch} from "vue";
+  import Card from "./components/Card.vue";
   
 
   const quizzes = ref(q);
   const search = ref("");
 
-  // making the search function
+  // making the search function using watch
   watch(search, () => {
     quizzes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
   })
@@ -21,13 +22,14 @@
     </header>
 
     <div class="options-container">
-      <div v-for="quiz in quizzes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz" />
+      <!-- <div v-for="quiz in quizzes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
@@ -64,30 +66,5 @@ header input{
   margin-top: 40px;
 }
 
-/* CARDS*/
-
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img{
-  width:100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text{
-  padding: 0 5px;
-}
-
-.card .card-text h2{
-  font-weight: bold;
-}
 
 </style>
