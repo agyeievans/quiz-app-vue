53d37b36472a54a6a3b9a2fe7a275ba8cd684f87
agyeievans <evanskofiagyei06@gmail.com>
2023-04-07 13:07:34 +0000

    import ref and useRoute

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,13 +1,24 @@
 <script setup>
 import question from '../components/question.vue';
 import quizheader from '../components/quizheader.vue';
+import { useRoute } from 'vue-router';
+import { ref } from 'vue';
+import quizzes from "../data/quizzes.json"
+
+const route = useRoute()
+
+const quizId = parseInt(route.params.id)
+
+const quiz = quizzes.find(q => q.id === quizId)
+
+const currentQuestionIndex = ref(0)
 </script>
 
 <template>
     <div>
         <quizheader />
         <div>
-            <question />
+            <question :question="quiz.questions[currentQuestionIndex]"/>
         </div>
     </div>
 </template>
