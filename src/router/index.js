6e6ec5e977ff74a1a95d0bf78959af7d18e6bde5
agyeievans <evanskofiagyei06@gmail.com>
2023-04-07 12:05:36 +0000

    added quizview and routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 import QuizzesView from "../views/QuizzesView.vue"
+import QuizView from "../views/QuizView.vue"
 
 const router =createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router =createRouter({
             path: "/",
             name: "quizzes",
             component: QuizzesView
+        },
+        {
+            path: "/quiz/:id",
+            name: "quiz",
+            component: QuizView
         }
     ]
 })
